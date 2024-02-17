4e77f3270094086d502c2ceba7674e746eea6c22
Iain Sandoe <iain@sandoe.co.uk>
2024-02-17 10:01:45 +0000

    testsuite: Temporary test skips for PR113150
    gcc/testsuite/ChangeLog:
    
    	* c-c++-common/analyzer/fd-glibc-byte-stream-socket.c:
    	* c-c++-common/analyzer/fd-manpage-getaddrinfo-client.c:
    	* c-c++-common/analyzer/fd-mappage-getaddrinfo-server.c:
    	* c-c++-common/analyzer/fd-symbolic-socket.c:
    	* gcc.dg/analyzer/fd-glibc-byte-stream-connection-server.c:
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/testsuite/c-c++-common/analyzer/fd-manpage-getaddrinfo-server.c
+++ gcc/testsuite/c-c++-common/analyzer/fd-manpage-getaddrinfo-server.c
@@ -31,7 +31,7 @@ the source, must acknowledge the copyright and authors of this work.
 /* Needed on some targets until we have exception-handling working (PR 111475).  */
 /* { dg-additional-options "-fno-exceptions" } */
 
-/* { dg-skip-if "" { hppa*-*-hpux* powerpc*-*-aix* } } */
+/* { dg-skip-if "PR113150" { hppa*-*-hpux* powerpc*-*-aix* *-*-darwin* } } */
 
 #include <sys/types.h>
 #include <stdio.h>
