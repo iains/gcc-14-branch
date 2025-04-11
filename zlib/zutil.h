8fbe7a7dd3471ff379f84097e19490571c70ed31
DesWurstes <DesWurstes@users.noreply.github.com>
2025-04-11 12:15:55 +0100

    take even less risk

--- zlib/zutil.h
+++ zlib/zutil.h
@@ -130,7 +130,7 @@ extern z_const char * const z_errmsg[10]; /* indexed by 2-zlib_error */
 #  endif
 #endif
 
-#if defined(MACOS)
+#if defined(MACOS) || defined(TARGET_OS_MAC)
 #  define OS_CODE  7
 #endif
 
